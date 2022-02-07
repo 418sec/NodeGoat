127008c53956a6f6d6d02675efc8272b1f9019fa
Adam Nygate <adam@418sec.com>
2022-02-07 16:12:44 +0000

    feat(screen): test annotations and outputs

--- .github/workflows/screen/index.js
+++ .github/workflows/screen/index.js
@@ -1,1 +1,7 @@
-console.log("Hello, World!");
+const core = require("@actions/core");
+
+const result = "Hello, World!";
+console.log(result);
+
+core.setOutput("result", result);
+core.notice(result);
